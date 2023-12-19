2ec14fe6d02900a315f1cc3113ebd82a54e4b132
Grupo1063 <grupo106323@gmail.com>
2023-12-19 14:21:23 -0500

    Registro error

---
+++ Control/registro.py
@@ -0,0 +1,4 @@
+def registro(nombres, fecha, rol, email, pass_1):
+    print(f"{nombres}, \n{fecha}, \n{rol}, \n{email}, \n{pass_1} \n Registro exitoso")
+    
+print("Si vale")
--- Vista/ejecutador.py
+++ Vista/ejecutador.py
@@ -1,9 +1,23 @@
-            
+import Control.registro as rg
+
 def menu():
     print("\n\t\tMenu")
     print("1) Reguistrar")
     print("2) Iniciar Sesion")
     print("3) Salir")
+
+def reg():
+    print("Registro")
+    nombre = input("Ingrese su Nombre")
+    apellido = input("Ingrese su Apellido")
+    fecha = input("Ingrese su fecha de nacimiento")
+    email = input("Ingrese su Correo Electronico")
+    pass_1 = input("Ingrese su Contrasena ")
+    pass_2 = input("Confirmar Contrasena")
+    if (pass_1 == pass_2):
+        nombre_c = nombre + apellido
+        rg.registro(nombre_c, fecha, 1, email, pass_1 )
+        
     
 def inicio():
     i=0
@@ -12,7 +26,7 @@ def inicio():
         op = int(input("Ingrese una opcion \n>"))
         match op:
             case 1:
-                print("Eat it raw!")
+                reg()               
             case 2:
                 print("Make juice!")
             case 3:
